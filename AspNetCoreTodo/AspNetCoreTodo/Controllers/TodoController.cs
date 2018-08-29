9b7edded9b43e257683986009361882ced370020
Rachana Acharya <rasnacharya@gmail.com>
2018-08-29 14:01:18 -0600

    Add a dependancy to the ITodoItemService

--- AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index()
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
         {
+            _todoItemService = todoItemService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemAsync();
             // Get to-do items from database
             // Put items into a model
             // Render view using the model
