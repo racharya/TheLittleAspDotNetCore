7392636f7e69948b95fdd0c7f4ec78502e8e3174
Rachana Acharya <rasnacharya@gmail.com>
2018-09-01 13:08:05 -0600

    Add a DbSet and OnModelCreating method

--- AspNetCoreTodo/AspNetCoreTodo/Data/ApplicationDbContext.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AspNetCoreTodo.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,15 @@ namespace AspNetCoreTodo.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        
+        // DbSet represents a table in the database. By saying DbSet<TodoItem> Items we 
+        // are telling EFC that we want to store TodoItem entities in a table called
+        // Items.
+        public DbSet<TodoItem> Items {get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            base.OnModelCreating(builder);
         }
     }
 }
