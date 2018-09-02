d1d76f2595ba57409a1c8ef9c29c7a34b224aed3
Rachana Acharya <rasnacharya@gmail.com>
2018-09-02 12:37:02 -0600

    Format code, add a new service method

--- AspNetCoreTodo/AspNetCoreTodo/Services/TodoItemService.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Services/TodoItemService.cs
@@ -20,7 +20,8 @@ namespace AspNetCoreTodo.Services
             return await _context.Items.Where(x => x.IsDone == false).ToArrayAsync();
         }
 
-        public async Task<bool> AddItemAsync(TodoItem newItem){
+        public async Task<bool> AddItemAsync(TodoItem newItem)
+        {
             newItem.Id = Guid.NewGuid();
             newItem.IsDone = false;
             newItem.DueAt = DateTimeOffset.Now.AddDays(3);
@@ -29,6 +30,17 @@ namespace AspNetCoreTodo.Services
 
             var saveResult = await _context.SaveChangesAsync();
             return saveResult == 1;
+        }
+
+        public async Task<bool> MarkDoneAsync(Guid id)
+        {
+            var item = await _context.Items.Where(x => x.Id == id).SingleOrDefaultAsync();
+            if(item == null) return false;
+
+            item.IsDone = true;
+
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1;      
         }
     }
 }
