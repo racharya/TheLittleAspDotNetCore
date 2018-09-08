1856400e5f65a1315ac4bc568c1e3adc79075eb6
Rachana Acharya <rasnacharya@gmail.com>
2018-09-07 23:36:59 -0600

    Fix dependencies, install packages

--- AspNetCoreTodo/AspNetCoreTodo.IntegrationTests/TestFixture.cs
+++ AspNetCoreTodo/AspNetCoreTodo.IntegrationTests/TestFixture.cs
@@ -5,31 +5,37 @@ using System.Net.Http;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
+
+
 namespace AspNetCoreTodo.IntegrationTests
 {
-public class TestFixture : IDisposable
-{
-private readonly TestServer _server;
-public HttpClient Client { get; }
-public TestFixture()
-{
-    var builder = new WebHostBuilder()
-        .UseStartup<AspNetCoreTodo.Startup>()
-        .ConfigureAppConfiguration((context, config) =>
-            {
-                config.SetBasePath(Path.Combine(
-                    Directory.GetCurrentDirectory(),
-                    "..\\..\\..\\..\\AspNetCoreTodo"));
-                config.AddJsonFile("appsettings.json");
-            });
-        _server = new TestServer(builder);
-        Client = _server.CreateClient();
-        Client.BaseAddress = new Uri("http://localhost:8888");
-}
-public void Dispose()
-{
-    Client.Dispose();
-    _server.Dispose();
-}
-}
+    public class TestFixture : IDisposable  
+    {
+        private readonly TestServer _server;
+
+        public TestFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseStartup<AspNetCoreTodo.Startup>()
+                .ConfigureAppConfiguration((context, configBuilder) =>
+                {
+                    configBuilder.SetBasePath(Path.Combine(
+                        Directory.GetCurrentDirectory(), "..\\..\\..\\..\\AspNetCoreTodo"));
+
+                    configBuilder.AddJsonFile("appsettings.json");
+                });
+            _server = new TestServer(builder);
+
+            Client = _server.CreateClient();
+            Client.BaseAddress = new Uri("http://localhost:5000");
+        }
+
+        public HttpClient Client { get; }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            _server.Dispose();
+        }
+    }
 }
